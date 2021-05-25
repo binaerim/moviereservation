400f6bcb6b7544c5fab369b9e16abce792571667
hoho <kingsbae@gmail.com>
2021-05-25 17:30:07 +0900

    dskjl

--- payment/src/main/java/moviereservation/PolicyHandler.java
+++ payment/src/main/java/moviereservation/PolicyHandler.java
@@ -9,25 +9,27 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PolicyHandler{
-    @Autowired PaymentRepository paymentRepository;
+public class PolicyHandler {
+    @Autowired
+    PaymentRepository paymentRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverCancelled_PayCancel(@Payload Cancelled cancelled){
+    public void wheneverCancelled_PayCancel(@Payload Cancelled cancelled) {
 
-        if(!cancelled.validate()) return;
+        if (!cancelled.validate())
+            return;
 
         System.out.println("\n\n##### listener PayCancel : " + cancelled.toJson() + "\n\n");
 
-        // Sample Logic //
         Payment payment = new Payment();
+        payment.setPaymentStatus(cancelled.getStatus());
+        payment.setReservationId(cancelled.getId());
         paymentRepository.save(payment);
-            
+
     }
 
-
     @StreamListener(KafkaProcessor.INPUT)
-    public void whatever(@Payload String eventString){}
-
+    public void whatever(@Payload String eventString) {
+    }
 
 }
