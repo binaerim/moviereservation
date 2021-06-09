ebad9ab1dce3f2b3b082506dddf88c9f0e1e6865
binaerim <80744169+binaerim@users.noreply.github.com>
2021-06-10 05:27:46 +0900

    Update ReservationApplication.java

--- reservation/src/main/java/moviereservation/ReservationApplication.java
+++ reservation/src/main/java/moviereservation/ReservationApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-
-@EnableHystrix
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
